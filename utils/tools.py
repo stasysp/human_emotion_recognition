89befd838838177adc5aadcc0574a84c91ae0114
iamilyasedunov <iliasedunov@yandex.ru>
2020-05-19 12:26:40 +0300

    tools for cli app

--- utils/tools.py
+++ utils/tools.py
@@ -17,7 +17,7 @@ labels = [
 
 def get_dirs_tree(path_to_destination_dataset):
     dir_names = labels
-    for name_dir in ['\\train', '\\val']:
+    for name_dir in ['/train', '/val']:
         path_to_main_dirs = path_to_destination_dataset + name_dir
         try:
             os.makedirs(path_to_main_dirs)
@@ -26,7 +26,7 @@ def get_dirs_tree(path_to_destination_dataset):
         else:
             print ("Successfully created the directory %s" % path_to_main_dirs)
         for name_dir_emotions in dir_names:
-            path_to_inner_dirs = path_to_main_dirs + "\\" + name_dir_emotions
+            path_to_inner_dirs = path_to_main_dirs + "/" + name_dir_emotions
             try:
                 os.makedirs(path_to_inner_dirs)
             except OSError:
@@ -36,18 +36,27 @@ def get_dirs_tree(path_to_destination_dataset):
     
 
 def get_train_val(path_to_csv, path_to_dataset, path_to_destination_dataset):
+    number_of_pngs = {'val' : 0, 'train' : 0, 'positive' : 0, 'neutral' : 0, 'negative' : 0, 'skip' : 0, 'speech' : 0, 'unknown' : 0,}
     df = pd.read_csv(path_to_csv, encoding="Windows-1251")
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
     get_dirs_tree(path_to_destination_dataset)
     for index, row in df.iterrows():
-        path_to_txt = path_to_dataset + row['path'][2:]
-        if (index > 10): break
-    
-        main_dir = '\\val' if index % 3 == 0 else '\\train'
-        dest_file_path = path_to_destination_dataset + main_dir + "\\" + row['emotions'] + "\\"
-        #print(row['emotions'], dest_file_path)
-        copy(path_to_txt, dest_file_path)
+        if (index == 8000): break
+        path_to_txt = path_to_dataset + row['path'][2:].replace('\\', '/')
+        path_to_png = path_to_txt[:-3] + "png"
+        if (not os.path.exists(path_to_png)):
+            continue
 
+        main_dir = 'val' if index % 3 == 0 else 'train'
+
+        number_of_pngs[main_dir]+=1
+        number_of_pngs[row['emotions']]+=1
+
+        dest_file_path = path_to_destination_dataset + "/" + main_dir + "/" + row['emotions'] + "/"
+        #print(row['emotions'], dest_file_path)
+        copy(path_to_png, dest_file_path)
+    print("Number of pngs copied to dataset: ")
+    print(number_of_pngs)
 
 def get_stat_from_initial_results(path_to_files):
 
