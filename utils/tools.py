b29288abce247863442a454077174d924ca01660
iamilyasedunov <iliasedunov@yandex.ru>
2020-05-21 08:44:56 +0300

    multi format tool extension

--- utils/tools.py
+++ utils/tools.py
@@ -35,18 +35,19 @@ def get_dirs_tree(path_to_destination_dataset):
                 print ("Successfully created the directory %s" % path_to_inner_dirs)
     
 
-def get_train_val(path_to_csv, path_to_dataset, path_to_destination_dataset):
+def get_train_val(path_to_csv, path_to_dataset, path_to_destination_dataset, number_of_samples, number_of_samples_per_class, extension):
     number_of_pngs = {'val' : 0, 'train' : 0, 'positive' : 0, 'neutral' : 0, 'negative' : 0, 'skip' : 0, 'speech' : 0, 'unknown' : 0,}
     df = pd.read_csv(path_to_csv, encoding="Windows-1251")
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
     get_dirs_tree(path_to_destination_dataset)
     for index, row in df.iterrows():
-        if (index == 8000): break
         path_to_txt = path_to_dataset + row['path'][2:].replace('\\', '/')
-        path_to_png = path_to_txt[:-3] + "png"
-        if (not os.path.exists(path_to_png)):
-            continue
+        path_to_png = path_to_txt[:-3] + extension[1:]
 
+        if (not os.path.exists(path_to_png) or (number_of_pngs[row['emotions']]>number_of_samples_per_class)):
+            continue
+        if (number_of_pngs['train'] + number_of_pngs['val'] > number_of_samples):
+            break
         main_dir = 'val' if index % 3 == 0 else 'train'
 
         number_of_pngs[main_dir]+=1
